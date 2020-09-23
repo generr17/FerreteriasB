1af07648d26adeb3edff267a30747f1ebe0fa4f3
grubira4167 <grubira4167@uta.edu.ec>
2020-09-23 17:48:12 -0500

    productos

---
+++ catalogo/compresores.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+    <title>Compresores | Ferreterías Bolívar</title>
+    <meta content="" name="descriptison">
+    <meta content="" name="keywords">
+  
+    <!-- Favicons -->
+    <link href="https://ferreteriasbolivar.com/assets/img/favicon.png" rel="icon">
+    <link href="https://ferreteriasbolivar.com/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  
+    <!-- Vendor CSS Files -->
+    <link href="https://ferreteriasbolivar.com/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/aos/aos.css" rel="stylesheet">
+   
+  
+    <!-- Template Main CSS File -->
+    <link href="https://ferreteriasbolivar.com/assets/css/style.css" rel="stylesheet">
+  
+</head>
+<body>
+    <!--header-->
+        <?php include('../header.php'); ?>
+  <!--endheader-->
+<div class="fondoa">
+      <!-- ======= Latest Properties Section ======= -->
+    
+      <main id="main">
+      <!-- ======= Property Grid ======= -->
+    
+    <section class="property-grid grid">
+    <div class="section-title">
+        <h2>Cerraduras</h2>
+      </div>
+        <div class="container">
+          <div class="row">
+            
+          <div class="col-md-4">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/Maquinaria/Compresores/1.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                    
+                    <div class="card-footer-a" style="bottom:-420px">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Compresor Truper</h4>
+                    </div>     
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/Maquinaria/Compresores/2.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                    
+                    <div class="card-footer-a" style="bottom:-420px">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Compresor Rong Long</h4>
+                    </div>     
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-md-4">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/Maquinaria/Compresores/3.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                    
+                    <div class="card-footer-a" style="bottom:-420px">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Compresor BP</h4>
+                    </div>     
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/Maquinaria/Compresores/4.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                    
+                    <div class="card-footer-a" style="bottom:-420px">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Compresor incco</h4>
+                    </div>     
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+              
+            <div class="col-md-4">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/Maquinaria/Compresores/5.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                    
+                    <div class="card-footer-a" style="bottom:-420px">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Compresor Powermate</h4>
+                    </div>     
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-12">
+              <nav class="pagination-a">
+                <ul class="pagination justify-content-end">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1">
+                      <span class="ion-ios-arrow-back"></span>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </section><!-- End Property Grid Single-->
+  
+    </main><!-- End #main -->
+   </div> 
+
+    <!-- ======= Footer ======= -->
+    <?php include('../footer.php'); ?>
+    <!-- End  Footer -->
+  
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+  
+    <!-- Vendor JS Files -->
+    <script src="https://ferreteriasbolivar.com/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/php-email-form/validate.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/counterup/counterup.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/venobox/venobox.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/aos/aos.js"></script>
+  
+    <!-- Template Main JS File -->
+    <script src="https://ferreteriasbolivar.com/assets/js/main.js"></script>
+</body>
+</html>
---
+++ catalogo/materialdeconstruccion.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+    <title>Material de construccion | Ferreterías Bolívar</title>
+    <meta content="" name="descriptison">
+    <meta content="" name="keywords">
+  
+    <!-- Favicons -->
+    <link href="https://ferreteriasbolivar.com/assets/img/favicon.png" rel="icon">
+    <link href="https://ferreteriasbolivar.com/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  
+    <!-- Vendor CSS Files -->
+    <link href="https://ferreteriasbolivar.com/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="https://ferreteriasbolivar.com/assets/vendor/aos/aos.css" rel="stylesheet">
+   
+  
+    <!-- Template Main CSS File -->
+    <link href="https://ferreteriasbolivar.com/assets/css/style.css" rel="stylesheet">
+  
+</head>
+<body>
+    <!--header-->
+<?php include('../header.php'); ?>
+  <!--endheader-->
+<div class="fondoa">
+      <!-- ======= Latest Properties Section ======= -->
+    
+      <main id="main">
+      <!-- ======= Property Grid ======= -->
+     
+    <section class="property-grid grid">
+    
+   
+        <div class="container" >
+        <div class="section-title">
+        <h2>Gypsum</h2>
+      </div>
+          <div class="row">
+            
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/Tuber%C3%ADayaccesorios/tuberiayaccesoriosdeprolipoleno.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                  
+                    <div class="card-footer-a">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Descripción</h4>
+                        Marca: Plastigama
+                          Pintura Arquitectónica tipo vinil-acrílica diseñada para la protección 
+                          y decoración de superficies de fibrocement, tanto exterior como interior.<br>
+                    </div> 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+         
+            </div>
+            <div class="section-title">
+              <h2>Barillas</h2>
+            </div>
+            <div class="row">
+            
+          
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/MaterialdeConstruccion/Barilla/Novacero/1.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                  
+                   
+                    <div class="card-footer-a">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Descripción</h4>
+                          Pintura Arquitectónica tipo vinil-acrílica diseñada para la protección 
+                          y decoración de superficies de fibrocement, tanto exterior como interior.<br>
+                    </div> 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/Tuber%C3%ADayaccesorios/tuberiayaccesoriosdecobre.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                  
+                   
+                    <div class="card-footer-a">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Descripción</h4>
+                          Pintura Arquitectónica tipo vinil-acrílica diseñada para la protección 
+                          y decoración de superficies de fibrocement, tanto exterior como interior.<br>
+                    </div> 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            <div class="section-title">
+              <h2>Escaleras</h2>
+            </div>
+            <div class="row">
+            
+          
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/MaterialdeConstruccion/Escaleras/century.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                  
+                   
+                    <div class="card-footer-a">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Descripción</h4>
+                          Pintura Arquitectónica tipo vinil-acrílica diseñada para la protección 
+                          y decoración de superficies de fibrocement, tanto exterior como interior.<br>
+                    </div> 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+              <div class="card-box-a card-shadow">
+                <div class="img-box-a">
+                  <img src="https://ferreteriasbolivar.com/assets/img/productos/MaterialdeConstruccion/Escaleras/cuprum.png" alt="" class="img-a img-fluid">
+                </div>
+                <div class="card-overlay">
+                  <div class="card-overlay-a-content">
+                  
+                   
+                    <div class="card-footer-a">
+                    <div class="descripcion">
+                    <h4 class="card-info-title">Descripción</h4>
+                          Pintura Arquitectónica tipo vinil-acrílica diseñada para la protección 
+                          y decoración de superficies de fibrocement, tanto exterior como interior.<br>
+                    </div> 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+        </div>
+       
+      </section><!-- End Property Grid Single-->
+  
+    </main><!-- End #main -->
+   </div> 
+
+    <!-- ======= Footer ======= -->
+    
+    <?php include('../footer.php'); ?>
+    <!-- End  Footer -->
+  
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+  
+    <!-- Vendor JS Files -->
+    <script src="https://ferreteriasbolivar.com/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/php-email-form/validate.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/counterup/counterup.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/venobox/venobox.min.js"></script>
+    <script src="https://ferreteriasbolivar.com/assets/vendor/aos/aos.js"></script>
+  
+    <!-- Template Main JS File -->
+    <script src="https://ferreteriasbolivar.com/assets/js/main.js"></script>
+</body>
+</html>
